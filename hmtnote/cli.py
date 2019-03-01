501f5e248c5bae183ce10e1357904183dcdfdd7f
Roberto Preste <roberto.preste@gmail.com>
2019-03-01 10:53:46 +0100

    Add CLI for annotator

--- hmtnote/cli.py
+++ hmtnote/cli.py
@@ -3,16 +3,39 @@
 # Created by Roberto Preste
 import sys
 import click
+from .classes import Annotator
 
 
 @click.command()
-def main(args=None):
-    """Console script for hmtnote."""
-    click.echo("Replace this message by putting your code into "
-               "hmtnote.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.argument("input_vcf")
+@click.argument("output_vcf")
+@click.option("--basic", "-b", is_flag=True, default=False,
+              help="""Annotate VCF using basic information (locus, pathogenicity, Clinvar and dbSNP 
+              IDs, etc.) (default: False)""")
+@click.option("--variab", "-v", is_flag=True, default=False,
+              help="""Annotate VCF using variability information (nucleotide and aminoacid 
+              variability, allele frequencies) (default: False)""")
+@click.option("--predict", "-p", is_flag=True, default=False,
+              help="""Annotate VCF using predictions information (from MutPred, Panther, Polyphen 
+              and other resources) (default: False)""")
+@click.version_option()
+def main(input_vcf, output_vcf, basic, variab, predict):
+    """
+    Annotate a VCF file using data from HmtVar.
+
+    If neither --basic, --variab nor --predict are
+    provided, they will all default to True, and the VCF will be annotated using all the available
+    information.
+    """
+
+    if not basic and not variab and not predict:
+        basic, variab, predict = True, True, True
+
+    vcf = Annotator(input_vcf, output_vcf, basic, variab, predict)
+    vcf.annotate()
+
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(main())
