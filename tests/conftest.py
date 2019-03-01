c3fc37aa84fa23f4665d2316666cf9aeb6c07cae
Roberto Preste <roberto.preste@gmail.com>
2019-03-01 20:26:34 +0100

    Add tests for CLI commands

---
+++ tests/conftest.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Roberto Preste
+import pytest
+import os
+
+TESTDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_hmtnote")
+
+
+@pytest.fixture
+def sample_vcf():
+    """Open the sample VCF file."""
+    vcf = os.path.join(TESTDIR, "HG00119_filt.vcf")
+    with open(vcf) as f:
+        yield f
+
+
+@pytest.fixture
+def sample_ann_vcf():
+    """Open the sample VCF file with full annotation."""
+    vcf = os.path.join(TESTDIR, "HG00119_ann.vcf")
+    with open(vcf) as f:
+        yield f
+
+
+@pytest.fixture
+def sample_ann_basic_vcf():
+    """Open the sample VCF file with basic annotation."""
+    vcf = os.path.join(TESTDIR, "HG00119_ann_basic.vcf")
+    with open(vcf) as f:
+        yield f
+
+
+@pytest.fixture
+def sample_ann_variab_vcf():
+    """Open the sample VCF file with variability annotation."""
+    vcf = os.path.join(TESTDIR, "HG00119_ann_variab.vcf")
+    with open(vcf) as f:
+        yield f
+
+
+@pytest.fixture
+def sample_ann_predict_vcf():
+    """Open the sample VCF file with predictions annotation."""
+    vcf = os.path.join(TESTDIR, "HG00119_ann_predict.vcf")
+    with open(vcf) as f:
+        yield f
--- tests/test_hmtnote.py
+++ tests/test_hmtnote.py
@@ -1,36 +1,71 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # Created by Roberto Preste
+import os
 import pytest
-
 from click.testing import CliRunner
-
-from hmtnote import hmtnote
+from hmtnote.hmtnote import annotate_vcf
 from hmtnote import cli
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+TESTDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_hmtnote")
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
+def test_cli():
+    """Test the main CLI command. Should exit with an error code 2, since no input and output files
+    are specified."""
     runner = CliRunner()
     result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'hmtnote.cli.main' in result.output
+    assert result.exit_code == 2
+
+
+def test_cli_help():
+    """Test the CLI help."""
+    runner = CliRunner()
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert 'Show this message and exit.' in help_result.output
+
+
+def test_cli_annotation(sample_ann_vcf):
+    """Test the full annotation of the sample VCF file."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main, [os.path.join(TESTDIR, "HG00119_filt.vcf"),
+                                      os.path.join(TESTDIR, "HG00119_test.vcf")])
+    assert result.exit_code == 0
+    with open(os.path.join(TESTDIR, "HG00119_test.vcf")) as out:
+        assert sample_ann_vcf.read() == out.read()
+
+
+def test_cli_annotation_basic(sample_ann_basic_vcf):
+    """Test the basic annotation of the sample VCF file."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main, [os.path.join(TESTDIR, "HG00119_filt.vcf"),
+                                      os.path.join(TESTDIR, "HG00119_test.vcf"),
+                                      "--basic"])
+    assert result.exit_code == 0
+    with open(os.path.join(TESTDIR, "HG00119_test.vcf")) as out:
+        assert sample_ann_basic_vcf.read() == out.read()
+
+
+def test_cli_annotation_variab(sample_ann_variab_vcf):
+    """Test the variability annotation of the sample VCF file."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main, [os.path.join(TESTDIR, "HG00119_filt.vcf"),
+                                      os.path.join(TESTDIR, "HG00119_test.vcf"),
+                                      "--variab"])
+    assert result.exit_code == 0
+    with open(os.path.join(TESTDIR, "HG00119_test.vcf")) as out:
+        assert sample_ann_variab_vcf.read() == out.read()
+
+
+def test_cli_annotation_predict(sample_ann_predict_vcf):
+    """Test the predictions annotation of the VCF file."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main, [os.path.join(TESTDIR, "HG00119_filt.vcf"),
+                                      os.path.join(TESTDIR, "HG00119_test.vcf"),
+                                      "--predict"])
+    assert result.exit_code == 0
+    with open(os.path.join(TESTDIR, "HG00119_test.vcf")) as out:
+        assert sample_ann_predict_vcf.read() == out.read()
+
