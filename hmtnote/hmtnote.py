c730bfc562fce1207eab89d6b5f301c9e20e2876
Roberto Preste <roberto.preste@gmail.com>
2019-03-01 10:54:09 +0100

    Add main script to perform annotation when importing HmtNote

--- hmtnote/hmtnote.py
+++ hmtnote/hmtnote.py
@@ -1,35 +1,58 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # Created by Roberto Preste
-import json
-import requests
-import vcf
+import argparse
+import sys
+from classes import Annotator
 
 
-# to update header with new infos:
-# pysam.VariantFile.header.info.add(id="TE", number=1, type="String", description="prova")
+parser = argparse.ArgumentParser(description="""Annotate a VCF file using information from
+HmtVar. If neither --basic, --variab nor --predict are provided, they will all default to True, and 
+the VCF will be annotated using all the available information. """)
+parser.add_argument("input_vcf", type=str, help="""Input VCF file to annotate.""")
+parser.add_argument("output_vcf", type=str, help="""File where the annotated VCF will be saved. """)
+parser.add_argument("-b", "--basic", action="store_true", dest="annot_basic",
+                    help="""Annotate VCF using basic information (locus, pathogenicity,
+                    Clinvar and dbSNP IDs, etc.) (default: False)""")
+parser.add_argument("-v", "--variab", action="store_true", dest="annot_variab",
+                    help="""Annotate VCF using variability information (nucleotide and
+                    aminoacid variability, allele frequencies) (default: False)""")
+parser.add_argument("-p", "--predict", action="store_true", dest="annot_predict",
+                    help="""Annotate VCF using predictions information (from MutPred,
+                    Panther, Polyphen and other resources) (default: False)""")
+
+args = parser.parse_args()
 
 
-def annotate_vcf(in_vcf, out_vcf):
-    reader = vcf.Reader(open(in_vcf, "r"))
+def annotate_vcf(input_vcf, output_vcf, basic, variab, predict):
+    """
+    Annotate a VCF file using information from HmtVar.
+
+    If neither --basic, --variab nor --predict are provided, they will all default to True, and
+    the VCF will be annotated using all the available information.
+    :param input_vcf: input VCF file to annotate
+    :param output_vcf: file where the annotated VCF will be saved
+    :param basic: annotate VCF using basic information (locus, pathogenicity, Clinvar and dbSNP IDs,
+    etc.) (default: False)
+    :param variab: annotate VCF using variability information (nucleotide and aminoacid variability,
+    allele frequencies) (default: False)
+    :param predict: annotate VCF using predictions information (from MutPred, Panther, Polyphen and
+    other resources) (default: False)
+    :return:
+    """
+    if not basic and not variab and not predict:
+        basic, variab, predict = True, True, True
+
+    vcf = Annotator(input_vcf, output_vcf, basic, variab, predict)
+    vcf.annotate()
+
+    return 0
 
 
+if __name__ == '__main__':
+    sys.exit(annotate_vcf(args.input_VCF, args.output_VCF,
+                          args.annot_basic, args.annot_variab, args.annot_predict))
 
-# def annotate_vcf(in_vcf, out_vcf):
-#     in_reader = vcf.Reader(open(in_vcf, "r"))
-#     out_writer = vcf.Writer(open(out_vcf, "w"), in_reader)
-#     for rec in in_reader:
-#         if rec.CHROM == "MT" and rec.ALT != [None]:
-#             if len(rec.ALT) == 1:
-#                 variant = f"{rec.REF}{rec.POS}{rec.ALT[0]}"
-#                 req = hmtvar_request(variant)
-#                 ann = hmtvar_parse(req)
-#                 rec.add_info("Locus", ann["Locus"])
-#                 rec.add_info("Aa_Change", ann["Aa_Change"])
-#                 rec.add_info("Pathogenicity", ann["Pathogenicity"])
-#                 rec.add_info("Disease_Score", ann["Disease_Score"])
-#                 out_writer.write_record(rec)
-#     out_writer.close()
 
 
 
