9e038f887ce6dbd9973031cbd6ef809fff815900
Roberto Preste <roberto.preste@gmail.com>
2019-03-03 17:05:09 +0100

    Add tests for imported module function

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,51 +5,51 @@ import pytest
 import os
 
 TESTDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_hmtnote")
+SAMPLE_VCF = os.path.join(TESTDIR, "HG00119_filt.vcf")
+ANN_VCF = os.path.join(TESTDIR, "HG00119_ann.vcf")
+ANN_BASIC_VCF = os.path.join(TESTDIR, "HG00119_ann_basic.vcf")
+ANN_CROSSREF_VCF = os.path.join(TESTDIR, "HG00119_ann_crossref.vcf")
+ANN_VARIAB_VCF = os.path.join(TESTDIR, "HG00119_ann_variab.vcf")
+ANN_PREDICT_VCF = os.path.join(TESTDIR, "HG00119_ann_predict.vcf")
 
 
 @pytest.fixture
 def sample_vcf():
     """Open the sample VCF file."""
-    vcf = os.path.join(TESTDIR, "HG00119_filt.vcf")
-    with open(vcf) as f:
+    with open(SAMPLE_VCF) as f:
         yield f
 
 
 @pytest.fixture
 def sample_ann_vcf():
     """Open the sample VCF file with full annotation."""
-    vcf = os.path.join(TESTDIR, "HG00119_ann.vcf")
-    with open(vcf) as f:
+    with open(ANN_VCF) as f:
         yield f
 
 
 @pytest.fixture
 def sample_ann_basic_vcf():
     """Open the sample VCF file with basic annotation."""
-    vcf = os.path.join(TESTDIR, "HG00119_ann_basic.vcf")
-    with open(vcf) as f:
+    with open(ANN_BASIC_VCF) as f:
         yield f
 
 
 @pytest.fixture
 def sample_ann_crossref_vcf():
     """Open the sample VCF file with crossref annotation."""
-    vcf = os.path.join(TESTDIR, "HG00119_ann_crossref.vcf")
-    with open(vcf) as f:
+    with open(ANN_CROSSREF_VCF) as f:
         yield f
 
 
 @pytest.fixture
 def sample_ann_variab_vcf():
     """Open the sample VCF file with variability annotation."""
-    vcf = os.path.join(TESTDIR, "HG00119_ann_variab.vcf")
-    with open(vcf) as f:
+    with open(ANN_VARIAB_VCF) as f:
         yield f
 
 
 @pytest.fixture
 def sample_ann_predict_vcf():
     """Open the sample VCF file with predictions annotation."""
-    vcf = os.path.join(TESTDIR, "HG00119_ann_predict.vcf")
-    with open(vcf) as f:
+    with open(ANN_PREDICT_VCF) as f:
         yield f
--- tests/test_hmtnote.py
+++ tests/test_hmtnote.py
@@ -4,11 +4,13 @@
 import os
 import pytest
 from click.testing import CliRunner
-# from hmtnote.hmtnote import annotate_vcf
+from hmtnote.hmtnote import annotate_vcf
 from hmtnote import cli
 
 
 TESTDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_hmtnote")
+SAMPLE_VCF = os.path.join(TESTDIR, "HG00119_filt.vcf")
+TEST_VCF = os.path.join(TESTDIR, "HG00119_test.vcf")
 
 
 def test_cli():
@@ -28,55 +30,81 @@ def test_cli_help():
 
 
 def test_cli_annotation(sample_ann_vcf):
-    """Test the full annotation of the sample VCF file."""
+    """Test the full annotation of the sample VCF file using the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main, [os.path.join(TESTDIR, "HG00119_filt.vcf"),
-                                      os.path.join(TESTDIR, "HG00119_test.vcf")])
+    result = runner.invoke(cli.main, [SAMPLE_VCF, TEST_VCF])
     assert result.exit_code == 0
-    with open(os.path.join(TESTDIR, "HG00119_test.vcf")) as out:
+    with open(TEST_VCF) as out:
         assert sample_ann_vcf.read() == out.read()
 
 
 def test_cli_annotation_basic(sample_ann_basic_vcf):
-    """Test the basic annotation of the sample VCF file."""
+    """Test the basic annotation of the sample VCF file using the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main, [os.path.join(TESTDIR, "HG00119_filt.vcf"),
-                                      os.path.join(TESTDIR, "HG00119_test.vcf"),
-                                      "--basic"])
+    result = runner.invoke(cli.main, [SAMPLE_VCF, TEST_VCF, "--basic"])
     assert result.exit_code == 0
-    with open(os.path.join(TESTDIR, "HG00119_test.vcf")) as out:
+    with open(TEST_VCF) as out:
         assert sample_ann_basic_vcf.read() == out.read()
 
 
 def test_cli_annotation_crossref(sample_ann_crossref_vcf):
-    """Test the crossref annotation of the sample VCF file."""
+    """Test the cross-reference annotation of the sample VCF file using the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main, [os.path.join(TESTDIR, "HG00119_filt.vcf"),
-                                      os.path.join(TESTDIR, "HG00119_test.vcf"),
-                                      "--crossref"])
+    result = runner.invoke(cli.main, [SAMPLE_VCF, TEST_VCF, "--crossref"])
     assert result.exit_code == 0
-    with open(os.path.join(TESTDIR, "HG00119_test.vcf")) as out:
+    with open(TEST_VCF) as out:
         assert sample_ann_crossref_vcf.read() == out.read()
 
 
 def test_cli_annotation_variab(sample_ann_variab_vcf):
-    """Test the variability annotation of the sample VCF file."""
+    """Test the variability annotation of the sample VCF file using the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main, [os.path.join(TESTDIR, "HG00119_filt.vcf"),
-                                      os.path.join(TESTDIR, "HG00119_test.vcf"),
-                                      "--variab"])
+    result = runner.invoke(cli.main, [SAMPLE_VCF, TEST_VCF, "--variab"])
     assert result.exit_code == 0
-    with open(os.path.join(TESTDIR, "HG00119_test.vcf")) as out:
+    with open(TEST_VCF) as out:
         assert sample_ann_variab_vcf.read() == out.read()
 
 
 def test_cli_annotation_predict(sample_ann_predict_vcf):
-    """Test the predictions annotation of the VCF file."""
+    """Test the predictions annotation of the sample VCF file using the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main, [os.path.join(TESTDIR, "HG00119_filt.vcf"),
-                                      os.path.join(TESTDIR, "HG00119_test.vcf"),
-                                      "--predict"])
+    result = runner.invoke(cli.main, [SAMPLE_VCF, TEST_VCF, "--predict"])
     assert result.exit_code == 0
-    with open(os.path.join(TESTDIR, "HG00119_test.vcf")) as out:
+    with open(TEST_VCF) as out:
+        assert sample_ann_predict_vcf.read() == out.read()
+
+
+def test_module_annotation(sample_ann_vcf):
+    """Test the full annotation of the sample VCF file using the imported module."""
+    annotate_vcf(SAMPLE_VCF, TEST_VCF)
+    with open(TEST_VCF) as out:
+        assert sample_ann_vcf.read() == out.read()
+
+
+def test_module_annotation_basic(sample_ann_basic_vcf):
+    """Test the basic annotation of the sample VCF file using the imported module."""
+    annotate_vcf(SAMPLE_VCF, TEST_VCF, basic=True)
+    with open(TEST_VCF) as out:
+        assert sample_ann_basic_vcf.read() == out.read()
+
+
+def test_module_annotation_crossref(sample_ann_crossref_vcf):
+    """Test the cross-reference annotation of the sample VCF file using the imported module."""
+    annotate_vcf(SAMPLE_VCF, TEST_VCF, crossref=True)
+    with open(TEST_VCF) as out:
+        assert sample_ann_crossref_vcf.read() == out.read()
+
+
+def test_module_annotation_variab(sample_ann_variab_vcf):
+    """Test the variability annotation of the sample VCF file using the imported module."""
+    annotate_vcf(SAMPLE_VCF, TEST_VCF, variab=True)
+    with open(TEST_VCF) as out:
+        assert sample_ann_variab_vcf.read() == out.read()
+
+
+def test_module_annotation_predict(sample_ann_predict_vcf):
+    """Test the predictions annotation of the sample VCF file using the imported module."""
+    annotate_vcf(SAMPLE_VCF, TEST_VCF, predict=True)
+    with open(TEST_VCF) as out:
         assert sample_ann_predict_vcf.read() == out.read()
 
