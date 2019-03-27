6a1644a9a05d2a50b71f0c9cce20870c80140173
Roberto Preste <roberto.preste@gmail.com>
2019-03-27 22:31:02 +0100

    Revert "Bump version: 0.2.0 → 0.2.1"
    This reverts commit 7430b292

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/robertopreste/hmtnote',
-    version='0.2.1',
+    version='0.2.0',
     zip_safe=False,
 )
