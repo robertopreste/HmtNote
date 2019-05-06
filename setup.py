940cac9cc81479ceb6e068acb19ebcb2901f1466
Roberto Preste <roberto.preste@gmail.com>
2019-05-06 10:22:51 +0200

    Bump version: 0.5.2 → 0.5.3

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(  # pragma: no cover
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/robertopreste/hmtnote',
-    version='0.5.2',
+    version='0.5.3',
     zip_safe=False,
 )
